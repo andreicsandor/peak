e8c730aa6d3b88ccea014d01395e430c0f9e0975
Andrei-Cristian S <andrei.sandor@outlook.com>
2024-08-07 21:13:41 +0300

    Add client project files

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html", // Add the path to your HTML files
+    "./src/**/*.html",
+    "./src/**/*.js",
+  ],
+  theme: {
+    // Your theme configuration
+  },
+  plugins: [
+    // Your plugins
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
