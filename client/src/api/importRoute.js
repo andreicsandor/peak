c82020017df842028e429818bdbcad9adb065d49
Andrei-Cristian S <andrei.sandor@outlook.com>
2024-08-07 21:31:37 +0300

    Add importer JS API file

---
+++ client/src/api/importRoute.js
@@ -0,0 +1,67 @@
+import { API_MAPS_URL } from "../config";
+import { ImportRoute } from "../dto/importRouteDTO";
+import { getAuthHeader } from "../utils/authUtils";
+
+export async function importRoute(file, routeId) {
+  const formData = new FormData();
+  formData.append("gpxFile", file);
+  if (routeId !== null) {
+    formData.append("routeId", routeId);
+  }
+
+  try {
+    const response = await fetch(
+      "http://localhost:8081/api/importing/import-route",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to import GPX file. Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data && data.routes && data.routes.length > 0) {
+      const routes = data.routes.map(
+        (route) =>
+          new ImportRoute({
+            id: route.id,
+            name: route.name,
+            waypoints: route.waypoints,
+            geoCoordinates: route.geoCoordinates,
+            createdTime: route.createdTime,
+            routeId: route.routeId,
+          })
+      );
+
+      const similarityResponse = await fetch(
+        `${API_MAPS_URL}/compare-routes?routeId=${routeId}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            ...getAuthHeader(),
+          },
+          body: JSON.stringify(routes[0]),
+        }
+      );
+
+      if (!similarityResponse.ok) {
+        throw new Error(`Failed to calculate similarity. Status: ${similarityResponse.status}`);
+      }
+
+      const similarityData = await similarityResponse.json();
+      const percentageSimilarity = similarityData.similarityPercentage;
+
+      return { routes, percentageSimilarity };
+    }
+
+    throw new Error("No routes found in the imported file.");
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("Failed to import GPX file.");
+  }
+}
