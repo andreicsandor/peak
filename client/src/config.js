afce91e6dff9ad3b99e273dd19504f4e5b13b901
Andrei-Cristian S <andrei.sandor@outlook.com>
2024-08-07 21:15:54 +0300

    Add client config file

---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+export const API_AUTH_URL = 'http://localhost:8079';
+export const API_USERS_URL = 'http://localhost:8079/auth/users';
+export const API_ROUTES_URL = 'http://localhost:8079/auth/routes';
+export const API_MAPS_URL = 'http://localhost:8079/auth/maps';
+export const API_WEATHER_URL = 'http://localhost:8083';
+export const API_ASSISTANT_URL = 'http://localhost:8079/auth/assistant';
