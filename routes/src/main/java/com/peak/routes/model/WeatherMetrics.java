c243ae000b79ee8e390ec10f13c723270334afae
Andrei-Cristian S <andrei.sandor@outlook.com>
2024-08-07 21:22:27 +0300

    Add weather metrics model

---
+++ routes/src/main/java/com/peak/routes/model/WeatherMetrics.java
@@ -0,0 +1,81 @@
+package com.peak.routes.model;
+
+import jakarta.persistence.Embeddable;
+
+import java.time.LocalDateTime;
+
+@Embeddable
+public class WeatherMetrics {
+    private String description;
+    private double temperature;
+    private double feelsLike;
+    private int humidity;
+    private int pressure;
+    private double windSpeed;
+    private int clouds;
+    private LocalDateTime scheduledTime;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getFeelsLike() {
+        return feelsLike;
+    }
+
+    public void setFeelsLike(double feelsLike) {
+        this.feelsLike = feelsLike;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public int getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(int clouds) {
+        this.clouds = clouds;
+    }
+
+    public LocalDateTime getScheduledTime() {
+        return scheduledTime;
+    }
+
+    public void setScheduledTime(LocalDateTime scheduledTime) {
+        this.scheduledTime = scheduledTime;
+    }
+}
