b7ec6d20a4fa10a94a50f62d7f7d6c2b66ffb92e
Andrei-Cristian S <andrei.sandor@outlook.com>
2024-08-07 21:17:15 +0300

    Add Assistant JS API file

---
+++ client/src/api/fetchTips.js
@@ -0,0 +1,26 @@
+import { API_ASSISTANT_URL } from "../config";
+import { getAuthHeader } from "../utils/authUtils";
+
+export async function fetchTips(routeData) {
+  const requestPayload = { routeData };
+
+  try {
+    const response = await fetch(`${API_ASSISTANT_URL}/get-tips`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        ...getAuthHeader(),
+      },
+      body: JSON.stringify(requestPayload),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to get recommendation. Network response was not ok.');
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Failed to get recommendation:", error);
+    throw new Error("Failed to get recommendation.");
+  }
+}
