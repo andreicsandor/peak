3ca5012a9258941d35f4595cf2a82abe9a153e96
Andrei-Cristian S <andrei.sandor@outlook.com>
2024-08-08 12:34:00 +0300

    Tweak Vite config file

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -2,6 +2,10 @@ import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
 export default {
+  server: {
+    host: 'localhost',
+    port: 5173,
+  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
