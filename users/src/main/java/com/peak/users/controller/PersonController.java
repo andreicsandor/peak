bd0604e757cc994207b52a5f9683a6c7b8b97e42
Andrei-Cristian S <andrei.sandor@outlook.com>
2024-08-13 00:06:29 +0300

    Tidy up code

--- users/src/main/java/com/peak/users/controller/PersonController.java
+++ users/src/main/java/com/peak/users/controller/PersonController.java
@@ -52,8 +52,6 @@ public class PersonController {
     public ResponseEntity<?> createPerson(@RequestBody NewPersonDTO newPersonDTO) {
         PersonDTO personDTO = personService.createPerson(newPersonDTO);
 
-        System.out.println(personDTO);
-
         if (personDTO != null) {
             return ResponseEntity.status(HttpStatus.CREATED).body(personDTO);
         } else {
