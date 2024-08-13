8cba871f18161707dba6c352651137bbe936ac30
Andrei-Cristian S <andrei.sandor@outlook.com>
2024-08-13 15:35:27 +0300

    Adjust calculateWeatherGrade method

--- client/src/utils/weatherUtils.js
+++ client/src/utils/weatherUtils.js
@@ -3,7 +3,7 @@ import { globalManager } from "../managers/globalManager.js";
 import { getCurrentDateTime } from "./datetimeUtils.js";
 import { toCamelCase } from "./interfaceUtils.js";
 
-const { weatherManager } = globalManager.getManagers(); 
+const { weatherManager } = globalManager.getManagers();
 
 export function getStoredWeatherForecast() {
   return weatherManager.getWeatherForecastData();
@@ -16,7 +16,7 @@ export function storeWeatherMetrics(forecast) {
 export async function fetchForecastData() {
   const { weatherManager } = globalManager.getManagers();
   const { lat, lon, datetime } = getCurrentDateTime();
-  
+
   try {
     const weatherData = await fetchWeather(lat, lon, datetime);
     weatherManager.setWeatherForecastData(weatherData);
@@ -77,12 +77,20 @@ export function calculateWeatherGrade(
 
   // Temperature (ideal range: 15°C to 25°C)
   if (temp < 15 || temp > 25) {
-    grade -= Math.min(Math.abs(temp - 20) / 5, 2);
+    if (temp > 30) {
+      grade -= Math.min(Math.abs(temp - 20) / 3, 4);
+    } else {
+      grade -= Math.min(Math.abs(temp - 20) / 5, 2);
+    }
   }
 
   // Feels Like Temperature (same range)
   if (feelsLike < 15 || feelsLike > 25) {
-    grade -= Math.min(Math.abs(feelsLike - 20) / 5, 2);
+    if (feelsLike > 30) {
+      grade -= Math.min(Math.abs(feelsLike - 20) / 3, 4);
+    } else {
+      grade -= Math.min(Math.abs(feelsLike - 20) / 5, 2);
+    }
   }
 
   // Humidity (ideal range: 30% to 60%)
